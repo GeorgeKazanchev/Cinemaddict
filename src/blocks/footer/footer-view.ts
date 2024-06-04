7ead96e1d6052c9192746c4a9a8e76758e8f6a44
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-06-04 19:17:33 +0300

    Переработан класс представления блока "footer"

--- src/blocks/footer/footer-view.ts
+++ src/blocks/footer/footer-view.ts
@@ -1,24 +1,39 @@
-import View from '../../interfaces/view';
+import AbstractView from '../../types/abstract-view';
 
-export default class FooterView implements View {
-    constructor(logo: Node, statistics: Node) {
-        this.logo = logo;
-        this.statistics = statistics;
+export default class FooterView extends AbstractView {
+    constructor(filmsCount: number) {
+        if (filmsCount < 0) {
+            throw new RangeError('Number of the films can not be negative.');
+        }
+
+        super();
+        this.filmsCount = filmsCount;
     }
 
-    logo: Node;
-    statistics: Node;
+    filmsCount: number;
+    template: string =
+        `<footer class="footer">
+            <section class="footer__logo logo logo--smaller">Cinemaddict</section>
+            <section class="footer__statistics">
+                <p></p>
+            </section>
+        </footer>`;
 
-    getTemplate(): Node {
-        const template = document.createElement('footer');
-        template.classList.add('footer');
-        return template;
-    }
-
-    getElement(): Node {
+    getElement(): Element {
         const element = this.getTemplate();
-        element.appendChild(this.logo);
-        element.appendChild(this.statistics);
+        this.setFilmsStatistics(element);
         return element;
+    }
+
+    private setFilmsStatistics(element: Element): void {
+        const statisticsElement = element.querySelector('.footer__statistics');
+        if (statisticsElement) {
+            const filmsCountElement = statisticsElement.querySelector('p');
+            if (filmsCountElement) {
+                filmsCountElement.textContent = this.filmsCount === 1
+                    ? `${this.filmsCount} movie inside`
+                    : `${this.filmsCount} movies inside`;
+            }
+        }
     }
 }
