14d4fe6f366e49058372293789d242bef06d91b4
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-06-10 21:01:55 +0300

    Начата работа над новой реализацией блока "statistics"
    Как и в случае с другими блоками, новая реализация предполагает наследование от класса AbstractView и использование шаблонизации.

--- src/blocks/film-details/film-details-view.ts
+++ src/blocks/film-details/film-details-view.ts
@@ -1,21 +1,88 @@
-import View from '../../interfaces/view';
+import AbstractView from '../../types/abstract-view';
+import Movie from '../../types/movie';
 
-export default class FilmDetailsView implements View {
-    constructor(innerContent: Node) {
-        this.innerContent = innerContent;
+export default class FilmDetailsView extends AbstractView {
+    constructor(film: Movie) {
+        super();
+        this.film = film;
     }
 
-    innerContent: Node;
+    film: Movie;
+    template: string =
+        `<section class="film-details">
+            <form class="film-details__inner" action="" method="get">
+                <div class="film-details__top-container">
+                    <div class="film-details__close">
+                        <button class="film-details__close-btn" type="button">close</button>
+                    </div>
+                    <div class="film-details__info-wrap">
+                        <div class="film-details__poster">
+                            <img class="film-details__poster-img" src="" alt="">
+                            <p class="film-details__age"></p>
+                        </div>
+                        <div class="film-details__info">
+                            <div class="film-details__info-head">
+                                <div class="film-details__title-wrap">
+                                    <h3 class="film-details__title"></h3>
+                                    <p class="film-details__title-original"></p>
+                                </div>
+                                <div class="film-details__rating">
+                                    <p class="film-details__total-rating"></p>
+                                </div>
+                            </div>
+                            <table class="film-details__table">
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Director</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Writers</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Actors</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Release Date</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Runtime</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Country</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                                <tr class="film-details__row">
+                                    <td class="film-details__term">Genres</td>
+                                    <td class="film-details__cell"></td>
+                                </tr>
+                            </table>
+                            <p class="film-details__film-description"></p>
+                        </div>
+                    </div>
+                    <section class="film-details__controls">
+                        <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
+                        <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-    getTemplate(): Node {
-        const template = document.createElement('section');
-        template.classList.add('film-details');
-        return template;
-    }
+                        <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
+                        <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-    getElement(): Node {
+                        <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
+                        <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
+                    </section>
+                </div>
+                <div class="film-details__bottom-container">
+
+                </div>
+            </form>
+        </section>`;
+
+    getElement(): Element {
         const element = this.getTemplate();
-        element.appendChild(this.innerContent);
+
         return element;
     }
 }
