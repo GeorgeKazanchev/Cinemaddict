6d5db9128db1eee78350d1395268da3c4d9d79b5
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-08-16 17:32:44 +0300

    Объект с информацией о пользователе перенесён в файл настроек; файл мок-данных удалён

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import ModelData from './ts/models-data/model-data';
 import Application from './ts/application';
-import { userData } from './mock-data';
+import { userData } from './settings';
 
 const data = new ModelData(userData);
 Application.showFilmsScreen(data);
--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,15 @@
+import UserData from './ts/types/user-data';
+
 export const FILMS_CHUNK_SIZE: number = 5;
 export const NEW_COMMENT_EMOJI_SIZE: number = 55;
 export const STATS_SHOWN_GENRES_COUNT: number = 4;
+
+export const userData: UserData = new UserData(
+    'Frank Sinatra',
+    25,
+    35,
+    20,
+    9350,
+    ['Horror'],
+    './img/bitmap@2x.png'
+);
