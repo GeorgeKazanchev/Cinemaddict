f235ded5b7bfb2ce78786984c69b388760859323
GeorgeKazanchev <georgekazanchev@gmail.com>
2025-01-13 14:05:18 +0300

    Реализовано переключение экранов из исходной вёрстки
    Переключение экранов приложения осуществляется двумя способами:
    1) по нажатию клавиш со стрелками влево и вправо на клавиатуре;
    2) по нажатию кнопок с изображениями стрелок в интерфейсе.
    
    Коммит является решением первого задания по проекту, которое служит для знакомства с вёрсткой и не несёт особой ценности. Впоследствии функциональность, добавленная в этом задании, будет удалена.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import webpack from 'webpack';
+import webpack, { ModuleOptions } from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 type EnvVariables = {
@@ -10,9 +11,21 @@ type EnvVariables = {
 export default (env: EnvVariables) => {
   const isProd = env.mode === 'production';
 
+  const buildLoaders = (): ModuleOptions['rules'] => {
+    const tsLoader = {
+      test: /\.ts/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    };
+
+    return [
+      tsLoader,
+    ];
+  };
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
-    entry: './src/js/main.js',
+    entry: './src/ts/index.ts',
     output: {
       filename: 'bundle.js',
       path: path.join(__dirname, './public'),
@@ -22,7 +35,19 @@ export default (env: EnvVariables) => {
       port: env.port,
       compress: false,
       open: true,
-    }
+    },
+    module: {
+      rules: buildLoaders(),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+        chunkFilename: 'css/[name].css',
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
   };
 
   return config;
